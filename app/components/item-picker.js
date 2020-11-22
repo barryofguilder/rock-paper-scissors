e382cbba6d8f0d5d08329311e1c2ee003d6568b3
Jason Barry <jbarry@hey.com>
2020-11-22 15:31:45 -0500

    Choose mode working

--- app/components/item-picker.js
+++ app/components/item-picker.js
@@ -10,12 +10,35 @@ const ROTATION_INTERVAL = 3;
 
 const ITEMS = ['rock', 'paper', 'scissors'];
 
+class Player {
+  @tracked playerNumber;
+  @tracked mode;
+  @tracked randomKey;
+  @tracked rockKey;
+  @tracked paperKey;
+  @tracked scissorKey;
+
+  constructor(playerNumber, mode) {
+    this.playerNumber = playerNumber;
+    this.mode = mode;
+
+    if (this.mode === 'random') {
+      this.randomKey = this.playerNumber === 1 ? 'Space' : 'Enter';
+    } else {
+      this.rockKey = this.playerNumber === 1 ? 'a' : 'h';
+      this.paperKey = this.playerNumber === 1 ? 's' : 'j';
+      this.scissorKey = this.playerNumber === 1 ? 'd' : 'k';
+    }
+  }
+}
+
 export default class ItemPickerComponent extends Component {
   @tracked interval = STARTING_INTERVAL;
   @tracked item = null;
   @tracked chosenItem = null;
   @tracked index = 0;
   @tracked counter = 0;
+  @tracked player;
 
   resetItem() {
     this.item = null;
@@ -51,20 +74,49 @@ export default class ItemPickerComponent extends Component {
   }
 
   @action
-  iterateItem() {
-    later(
-      this,
-      function () {
-        this.changeItem();
-      },
-      this.interval
-    );
+  setupAndStart() {
+    this.player = new Player(this.args.player, this.args.mode);
+
+    if (this.args.mode === 'random') {
+      later(
+        this,
+        function () {
+          this.changeItem();
+        },
+        this.interval
+      );
+    }
   }
 
   @action
   chooseItem() {
-    this.chosenItem = this.item;
+    if (this.args.mode === 'random') {
+      this.chosenItem = this.item;
+      this.args.onChoose(this.args.player, this.chosenItem);
+    }
+  }
 
-    this.args.onChoose(this.args.player, this.chosenItem);
+  @action
+  chooseRock() {
+    if (this.args.mode === 'choose') {
+      this.chosenItem = 'rock';
+      this.args.onChoose(this.args.player, this.chosenItem);
+    }
+  }
+
+  @action
+  choosePaper() {
+    if (this.args.mode === 'choose') {
+      this.chosenItem = 'paper';
+      this.args.onChoose(this.args.player, this.chosenItem);
+    }
+  }
+
+  @action
+  chooseScissor() {
+    if (this.args.mode === 'choose') {
+      this.chosenItem = 'scissors';
+      this.args.onChoose(this.args.player, this.chosenItem);
+    }
   }
 }
